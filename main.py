d70daed4227853d6d2617229232e2ef2c295a6e7
Manuel Navas <63240941+menavas01@users.noreply.github.com>
2025-07-04 12:12:08 -0300

    .

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def main():
+    ventana = tk.Tk()
+    ventana.title('Sistema de Inventario')
+    ventana.iconbitmap('assets/icon.ico')
+    ventana.resizable(0,0)
+    ventana.mainloop()
+        
+if __name__ == '__main__':
+    main()
